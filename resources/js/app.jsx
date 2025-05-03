4fcfc83bac104b5b2b47b819a6d490ebedfbe735
dmytro <dziumanenkod@gmail.com>
2025-05-03 19:12:52 +0100

    Init

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+console.log("App component loaded");
+function App() {
+    return (
+        <BrowserRouter>
+        </BrowserRouter>
+    );
+}
+
+export default App;

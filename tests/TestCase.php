79abdbef8fac3b3ca8e96513aa5d51b5ed0d4321
vladikslon <vladbrovi@gmail.com>
2025-05-05 18:33:33 +0100

    create unit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,30 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase {
+        // alias the trait’s method so we can override it
+        RefreshDatabase::runDatabaseMigrations as baseRunDatabaseMigrations;
+    }
+    use CreatesApplication;
+
+    /**
+     * Override the trait method so it only runs migrations
+     * from your app’s database/migrations folder.
+     */
+    protected function runDatabaseMigrations(): void
+    {
+        // drop & re-create all tables, then run only your migrations
+        $this->artisan('migrate:fresh', [
+            '--path' => 'database/migrations',
+        ])->run();
+
+        // reset the Artisan kernel so other commands still work
+        $this->app[Kernel::class]->setArtisan(null);
+    }
 }

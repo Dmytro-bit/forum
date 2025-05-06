f261376873cf2fd9b81d549accec8a1f94f65dc1
vladikslon <vladbrovi@gmail.com>
2025-05-06 23:30:28 +0100

    Revert "Merge remote-tracking branch 'origin/Vladik' into Vladik"
    This reverts commit 68646562e82e8a9583094d2c1d24084e837f4480, reversing
    changes made to 06577268ddd1c6e023984b7f559cc5211d3439ff.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,54 +3,42 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory’s corresponding model.
-     *
-     * @var string
+     * The current password being used by the factory.
      */
-    protected $model = \App\Models\User::class;
+    protected static ?string $password;
 
     /**
-     * Define the model’s default state.
+     * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'name'              => $this->faker->name(),
-            'email'             => $this->faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            // Use Hash::make or bcrypt to ensure it’s recognized as “hashed”
-            'password'          => Hash::make('password'),
-            'remember_token'    => Str::random(10),
-            // if you have an is_admin column on users:
-            'is_admin'          => false,
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the user’s email address should be unverified.
+     * Indicate that the model's email address should be unverified.
      */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
-        ]);
-    }
-
-    /**
-     * Optionally mark as admin.
-     */
-    public function admin(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'is_admin' => true,
         ]);
     }
 }
